6a11c89192295fc34581829b7be42714d987d3fa
unknown <izharusmani786@gmail.com>
2021-06-20 23:01:13 +0530

    Updated About Template

---
+++ wp-content/themes/ams/template-about.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+Template Name: About Us
+*/
+get_header() ?>
+
+<div class="inner_sec">
+	<div class="container">
+		<div class="row">
+			Content Goes Here...
+		</div>
+	</div>
+</div>
+
+<?php get_footer() ?>
